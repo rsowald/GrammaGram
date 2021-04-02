bdd6dd268c0b6d582fafe3204fb4adf9ae6eb5b8
Corbin Garner <corbingar@gmail.com>
2021-04-01 19:01:34 -0700

    modified:   db/schema.sql 	modified:   views/layouts/main.handlebars

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,5 @@
 DROP DATABASE IF EXISTS GrammagramDB;
-CREATE DATABASE GrammagramDB;
+CREATE DATABASE GrandmagramDB;
 
 
 
@@ -10,10 +10,24 @@ USE animals_db;
 
 CREATE TABLE users (
   id INTEGER(11) AUTO_INCREMENT NOT NULL,
-  
-  authorId INTEGER(11),
 
-  title VARCHAR(100),
+  otherdata INTEGER(11),
+
+  username VARCHAR(100),
+
+  PRIMARY KEY (id)
+);
+
+
+CREATE TABLE posts (
+
+  poster INTEGER(11) NOT NULL,
+
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+
+  title VARCHAR(256),
+
+  username VARCHAR(100),
 
   PRIMARY KEY (id)
 );
